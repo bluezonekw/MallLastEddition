cdba1503c0bcb3f40c0168393a6517f1e3ab70b9
MostafaMahmoud <drshdrsh26@yahoo.com>
2022-01-05 19:47:32 +0200

    Finish Mall in 2021

---
+++ Assets/OptionForAttribute.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionForAttribute : MonoBehaviour
+{
+    public ArabicText NameOption, PriceOption;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
